ee073703931ad54a0d95fefdc2df751dd45fad22
xx182pav <53460770+xx182pav@users.noreply.github.com>
2019-08-11 10:36:17 +0300

    Add files via upload

---
+++ js/ryaba.js
@@ -0,0 +1,54 @@
+const dataURL = "https://api.myjson.com/bins/jcmhn";
+const fields = [
+    "var1",
+    "var2",
+    "var3",
+    "var4",
+    "var5",
+    "var6",
+    "speach"
+]
+
+function getFormValues() {
+    let obj = {};
+
+    fields.forEach(function (field) {
+        obj[field] = $("input[name=" + field + "]")[0].value
+    })
+
+    return obj;
+}
+
+function handleButton() {
+    $.getJSON(dataURL, handleData);
+    $("form").hide();
+}
+
+function handleData(data) {
+    let message = "";
+
+    let values = getFormValues();
+
+    data["text"].forEach(function (line) {
+        for (key in values) {
+            line = line.replace("{" + key + "}", values[key]);
+        }
+
+        message = message + line + "<br>";
+    })
+
+    $("div#result").html(message);
+
+}
+
+
+
+function init() {
+    $("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
+
+
+
+
